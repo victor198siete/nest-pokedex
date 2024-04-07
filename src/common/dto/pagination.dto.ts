7aa6d8728daaf088d0601c415b30d21464284722
Victor Moreno <victor198siete@gmail.com>
2024-04-06 22:55:39 -0500

    Section 8 Get all pagination

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+
+import { IsNumber, IsOptional, IsPositive, Min } from "class-validator";
+
+
+export class PaginationDto {
+
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    @Min(1)
+    limit?: number;
+
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    offset?: number;
+
+}
